41a181ed0763a31088228d8a8d72f784217b14eb
Hermes Ojeda Ruiz <hermes.ojeda@logicalbricks.com>
2013-08-31 18:02:22 -0500

    Agregando la validación de detalles contra totales

--- models/details_validator.rb
+++ models/details_validator.rb
@@ -1,15 +1,21 @@
+require 'nokogiri'
+
 class DetailsValidator
   def initialize(xml)
     @xml = xml
   end
 
-  def validate_details
-    self.total_zero_with_zero_elements and self.total_no_zero_with_elements
+  def valid_without_details?
+    total_zero_with_zero_elements or total_no_zero_with_elements
   end
 
-  private
-
   def total_zero_with_zero_elements
+    extractor = XmlInformationExtractor.new(@xml)
+    extractor.total_zero? and extractor.number_of_elements == 0
+  end
 
+  def total_non_zero_with_elements
+    extractor = XmlInformationExtractor.new(@xml)
+    (not extractor.total_zero?) and extractor.number_of_elements > 0
   end
 end
