fea71a77065ec4a524874287bdb417db03eb4e28
Azarel Doroteo Pacheco <azareldp@gmail.com>
2013-09-01 18:14:27 -0500

    Change instance method to class method in verificador_xsd.

--- models/verificador_xsd.rb
+++ models/verificador_xsd.rb
@@ -1,28 +1,26 @@
-require 'nokogiri'
 require_relative 'xsd_factory'
 
 class VerificadorXsd
-  attr_reader :errores
-
-  def verificar(xml_path)
-    @xml = Nokogiri::XML(File.read(xml_path))
-    @errores = validar_xsd
-    @errores !~ /[^[:space:]]/
+  def self.validar(xml)
+    @xml = xml
+    validar_xsd
   end
 
   private #============== PRIVATE =================
 
-  attr_reader :xsd, :xml
-
-  def xsd
-    @xsd ||= XsdFactory.xsd(xml_version)
+  def self.xml
+    @xml
   end
 
-  def xml_version
+  def self.xsd
+    XsdFactory.xsd(xml_version)
+  end
+
+  def self.xml_version
     xml.at_xpath('//DeclaracionInformativaMensualIDE')[:version]
   end
 
-  def validar_xsd
-    xsd.validate(xml).map(&:message).join('|')
+  def self.validar_xsd
+    xsd.validate(xml).empty?
   end
 end
--- spec/verificador_xsd.rb
+++ spec/verificador_xsd.rb
@@ -1,61 +1,45 @@
 require 'spec_helper'
 require_relative '../models/verificador_xsd'
+require 'nokogiri'
 
 describe VerificadorXsd do
-  it "no regresa errores si se valida un xml con datos válido versión 1.1" do
+  it "regresa verdadero si se valida un xml con datos válido versión 1.1" do
     path = File.dirname(__FILE__) + '/files/mensual_datos_1_1.xml'
-    verificador = VerificadorXsd.new
-    verificador.verificar(path).should be_true
-    verificador.errores.should == ""
+    VerificadorXsd.validar(Nokogiri::XML(File.read(path))).should be_true
   end
 
-  it "no regresa errores si se valida un xml en ceros válido versión 1.1" do
+  it "regresa verdadero si se valida un xml en ceros válido versión 1.1" do
     path = File.dirname(__FILE__) + '/files/mensual_ceros_1_1.xml'
-    verificador = VerificadorXsd.new
-    verificador.verificar(path).should be_true
-    verificador.errores.should == ""
+    VerificadorXsd.validar(Nokogiri::XML(File.read(path))).should be_true
   end
 
-  it "no regresa errores si se valida un xml con datos válido versión 2.0" do
+  it "regresa verdadero si se valida un xml con datos válido versión 2.0" do
     path = File.dirname(__FILE__) + '/files/mensual_datos_2_0.xml'
-    verificador = VerificadorXsd.new
-    verificador.verificar(path).should be_true
-    verificador.errores.should == ""
+    VerificadorXsd.validar(Nokogiri::XML(File.read(path))).should be_true
   end
 
-  it "no regresa errores si se valida un xml en ceros válido versión 2.0" do
+  it "regresa verdadero si se valida un xml en ceros válido versión 2.0" do
     path = File.dirname(__FILE__) + '/files/mensual_ceros_2_0.xml'
-    verificador = VerificadorXsd.new
-    verificador.verificar(path).should be_true
-    verificador.errores.should == ""
+    VerificadorXsd.validar(Nokogiri::XML(File.read(path))).should be_true
   end
 
-  it "regresa errores si se valida un xml en ceros versión 1.1 que indica versión 2.0" do
+  it "regresa falso si se valida un xml en ceros versión 1.1 que indica versión 2.0" do
     path = File.dirname(__FILE__) + '/files/mensual_ceros_1_1_indica_2_0.xml'
-    verificador = VerificadorXsd.new
-    verificador.verificar(path).should be_false
-    verificador.errores.should == "Element 'Totales': The attribute 'importeSaldoPendienteRecaudar' is required but missing."
+    VerificadorXsd.validar(Nokogiri::XML(File.read(path))).should be_false
   end
 
-  it "regresa errores si se valida un xml en ceros versión 2.0 que indica versión 1.1" do
+  it "regresa falso si se valida un xml en ceros versión 2.0 que indica versión 1.1" do
     path = File.dirname(__FILE__) + '/files/mensual_ceros_2_0_indica_1_1.xml'
-    verificador = VerificadorXsd.new
-    verificador.verificar(path).should be_false
-    verificador.errores.should == "Element 'Totales', attribute 'importeSaldoPendienteRecaudar': The attribute 'importeSaldoPendienteRecaudar' is not allowed."
+    VerificadorXsd.validar(Nokogiri::XML(File.read(path))).should be_false
   end
 
-  it "regresa errores si se valida un xml con datos versión 1.1 que indica versión 2.0" do
+  it "regresa falso si se valida un xml con datos versión 1.1 que indica versión 2.0" do
     path = File.dirname(__FILE__) + '/files/mensual_datos_1_1_indica_2_0.xml'
-    verificador = VerificadorXsd.new
-    verificador.verificar(path).should be_false
-    verificador.errores.should == "Element 'PersonaFisica': The attribute 'telefono1' is required but missing.|Element 'PersonaFisica': The attribute 'telefono2' is required but missing.|Element 'PersonaFisica': Missing child element(s). Expected is ( numeroCuenta ).|Element 'DepositoEnEfectivo': The attribute 'saldoPendienteRecaudar' is required but missing.|Element 'Totales': The attribute 'importeSaldoPendienteRecaudar' is required but missing."
+    VerificadorXsd.validar(Nokogiri::XML(File.read(path))).should be_false
   end
 
-  it "regresa errores si se valida un xml en ceros versión 2.0 que indica versión 1.1" do
+  it "regresa falso si se valida un xml en ceros versión 2.0 que indica versión 1.1" do
     path = File.dirname(__FILE__) + '/files/mensual_datos_2_0_indica_1_1.xml'
-    verificador = VerificadorXsd.new
-    verificador.verificar(path).should be_false
-    verificador.errores.should == "Element 'PersonaFisica', attribute 'NumeroCliente': The attribute 'NumeroCliente' is not allowed.|Element 'PersonaFisica', attribute 'correoElectronico': The attribute 'correoElectronico' is not allowed.|Element 'PersonaFisica', attribute 'telefono1': The attribute 'telefono1' is not allowed.|Element 'PersonaFisica', attribute 'telefono2': The attribute 'telefono2' is not allowed.|Element 'numeroCuenta': This element is not expected. Expected is ( Domicilio ).|Element 'DepositoEnEfectivo', attribute 'saldoPendienteRecaudar': The attribute 'saldoPendienteRecaudar' is not allowed.|Element 'PersonaFisica', attribute 'NumeroCliente': The attribute 'NumeroCliente' is not allowed.|Element 'PersonaFisica', attribute 'correoElectronico': The attribute 'correoElectronico' is not allowed.|Element 'PersonaFisica', attribute 'telefono1': The attribute 'telefono1' is not allowed.|Element 'PersonaFisica', attribute 'telefono2': The attribute 'telefono2' is not allowed.|Element 'numeroCuenta': This element is not expected. Expected is ( Domicilio ).|Element 'DepositoEnEfectivo', attribute 'saldoPendienteRecaudar': The attribute 'saldoPendienteRecaudar' is not allowed.|Element 'PersonaFisica', attribute 'NumeroCliente': The attribute 'NumeroCliente' is not allowed.|Element 'PersonaFisica', attribute 'correoElectronico': The attribute 'correoElectronico' is not allowed.|Element 'PersonaFisica', attribute 'telefono1': The attribute 'telefono1' is not allowed.|Element 'PersonaFisica', attribute 'telefono2': The attribute 'telefono2' is not allowed.|Element 'numeroCuenta': This element is not expected. Expected is ( Domicilio ).|Element 'DepositoEnEfectivo', attribute 'saldoPendienteRecaudar': The attribute 'saldoPendienteRecaudar' is not allowed.|Element 'EnteroPropio', attribute 'fechaRecaudacion': The attribute 'fechaRecaudacion' is not allowed.|Element 'Totales', attribute 'importeSaldoPendienteRecaudar': The attribute 'importeSaldoPendienteRecaudar' is not allowed."
+    VerificadorXsd.validar(Nokogiri::XML(File.read(path))).should be_false
   end
-
 end
