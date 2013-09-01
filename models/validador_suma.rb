6f4e4a3efabe4a2c80f8196309cea035e708631c
Hermes Ojeda Ruiz <hermes.ojeda@logicalbricks.com>
2013-09-01 13:35:31 -0500

    Refactorizando el validador para hacerlo estático

--- models/validador_suma.rb
+++ models/validador_suma.rb
@@ -3,19 +3,15 @@ require 'ostruct'
 
 class ValidadorSuma
 
-  def initialize(xml)
-    @xml = xml
-  end
-
-  def verificar_totales
-    elementos = sumar_elementos
-    totales = @xml.css('Totales').first
+  def self.verificar_totales(xml)
+    elementos = sumar_elementos(xml)
+    totales = xml.css('Totales').first
     comparar_totales(totales, elementos)
   end
 
   private
 
-  def comparar_totales(totales, elementos_sumados)
+  def self.comparar_totales(totales, elementos_sumados)
     relacion_atributos = {
     'operacionesRelacionadas' => 'operacionesRelacionadas',
     'importeExcedenteDepositos' => 'montoExcedente',
@@ -29,31 +25,31 @@ class ValidadorSuma
     relacion_atributos.all?{ |k,v| totales[k].to_i == elementos_sumados[v] }
   end
 
-  def sumar_elementos
-    depositos = @xml.css('RegistroDeDetalle/DepositoEnEfectivo')
-    enteros = @xml.css('ReporteDeRecaudacionYEnteroDiaria/EnteroPropio')
-    enteros_otras_instituciones = @xml.css('ReporteDeRecaudacionYEnteroDiaria/EnteroDeOtrasInstituciones')
-    cheques = @xml.css('RegistroDeDetalle/ChequeDeCaja')
+  def self.sumar_elementos(xml)
+    depositos = xml.css('RegistroDeDetalle/DepositoEnEfectivo')
+    enteros = xml.css('ReporteDeRecaudacionYEnteroDiaria/EnteroPropio')
+    enteros_otras_instituciones = xml.css('ReporteDeRecaudacionYEnteroDiaria/EnteroDeOtrasInstituciones')
+    cheques = xml.css('RegistroDeDetalle/ChequeDeCaja')
     total = suma_depositos_efectivo(depositos).merge(suma_enteros(enteros + enteros_otras_instituciones)).merge(suma_cheques(cheques))
     total.merge('operacionesRelacionadas' => depositos.size)
   end
 
-  def suma_depositos_efectivo(registros)
+  def self.suma_depositos_efectivo(registros)
     atributos = %w{ montoExcedente impuestoDeterminado impuestoRecaudado recaudacionPendiente remanentePeriodosAnteriores saldoPendienteRecaudar }
     suma_registros = sumar_campos(registros, atributos)
   end
 
-  def suma_enteros(enteros)
+  def self.suma_enteros(enteros)
     atributos = %w{ impuestoEnterado }
     suma_enteros = sumar_campos(enteros, atributos)
   end
 
-  def suma_cheques(cheques)
+  def self.suma_cheques(cheques)
     atributos = %w{ montoCheque montoRecaudado }
     suma_cheques = sumar_campos(cheques, atributos)
   end
 
-  def sumar_campos(registros, atributos)
+  def self.sumar_campos(registros, atributos)
     suma_registros = {}
     inicializa_suma_depositos_efectivo(suma_registros, atributos)
     registros.each do |registro|
@@ -64,7 +60,7 @@ class ValidadorSuma
     suma_registros
   end
 
-  def inicializa_suma_depositos_efectivo(suma_registros, campos)
+  def self.inicializa_suma_depositos_efectivo(suma_registros, campos)
     campos.each do |campo|
       suma_registros[campo] = 0
     end
--- spec/validador_suma_spec.rb
+++ spec/validador_suma_spec.rb
@@ -6,25 +6,22 @@ describe ValidadorSuma do
     context 'version 2.0' do
       context 'con datos' do
         context 'válido' do
-          let(:validador){ValidadorSuma.new(Nokogiri::XML(File.read('spec/files/mensual_datos_2_0.xml')))}
-          it{ expect(validador.verificar_totales).to eq(true) }
+          it{ expect(ValidadorSuma.verificar_totales(Nokogiri::XML(File.read('spec/files/mensual_datos_2_0.xml')))).to eq(true) }
         end
 
         context 'inválido' do
-          let(:validador){ValidadorSuma.new(Nokogiri::XML(File.read('spec/files/mensual_ceros_con_detalles_2_0.xml')))}
-          it{ expect(validador.verificar_totales).to eq(false) }
+          let(:validador){}
+          it{ expect(ValidadorSuma.verificar_totales(Nokogiri::XML(File.read('spec/files/mensual_ceros_con_detalles_2_0.xml')))).to eq(false) }
         end
       end
 
       context 'sin datos' do
         context 'válido' do
-          let(:validador){ValidadorSuma.new(Nokogiri::XML(File.read('spec/files/mensual_ceros_2_0.xml')))}
-          it{ expect(validador.verificar_totales).to eq(true) }
+          it{ expect(ValidadorSuma.verificar_totales(Nokogiri::XML(File.read('spec/files/mensual_ceros_2_0.xml')))).to eq(true) }
         end
 
         context 'inválido' do
-          let(:validador){ValidadorSuma.new(Nokogiri::XML(File.read('spec/files/mensual_datos_sin_detalles_2_0.xml')))}
-          it{ expect(validador.verificar_totales).to eq(false) }
+          it{ expect(ValidadorSuma.verificar_totales(Nokogiri::XML(File.read('spec/files/mensual_datos_sin_detalles_2_0.xml')))).to eq(false) }
         end
       end
     end
@@ -32,25 +29,21 @@ describe ValidadorSuma do
     context 'version 1.1' do
       context 'con datos' do
         context 'válido' do
-          let(:validador){ValidadorSuma.new(Nokogiri::XML(File.read('spec/files/mensual_datos_1_1.xml')))}
-          it{ expect(validador.verificar_totales).to eq(true) }
+          it{ expect(ValidadorSuma.verificar_totales(Nokogiri::XML(File.read('spec/files/mensual_datos_1_1.xml')))).to eq(true) }
         end
 
         context 'inválido' do
-          let(:validador){ValidadorSuma.new(Nokogiri::XML(File.read('spec/files/mensual_ceros_con_detalles_1_1.xml')))}
-          it{ expect(validador.verificar_totales).to eq(false) }
+          it{ expect(ValidadorSuma.verificar_totales(Nokogiri::XML(File.read('spec/files/mensual_ceros_con_detalles_1_1.xml')))).to eq(false) }
         end
       end
 
       context 'sin datos' do
         context 'válido' do
-          let(:validador){ValidadorSuma.new(Nokogiri::XML(File.read('spec/files/mensual_ceros_1_1.xml')))}
-          it{ expect(validador.verificar_totales).to eq(true) }
+          it{ expect(ValidadorSuma.verificar_totales(Nokogiri::XML(File.read('spec/files/mensual_ceros_1_1.xml')))).to eq(true) }
         end
 
         context 'inválido' do
-          let(:validador){ValidadorSuma.new(Nokogiri::XML(File.read('spec/files/mensual_datos_sin_detalles_1_1.xml')))}
-          it{ expect(validador.verificar_totales).to eq(false) }
+          it{ expect(ValidadorSuma.verificar_totales(Nokogiri::XML(File.read('spec/files/mensual_datos_sin_detalles_1_1.xml')))).to eq(false) }
         end
       end
     end
