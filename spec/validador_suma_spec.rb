d2fccf9a797e6d7fe68794acd4a977506555cc54
Hermes Ojeda Ruiz <hermes.ojeda@logicalbricks.com>
2013-09-01 17:58:45 -0500

    Corrigiendo pruebas, y agregando las pruebas con declaración anual con datos y sin datos

--- spec/validador_suma_spec.rb
+++ spec/validador_suma_spec.rb
@@ -6,22 +6,22 @@ describe ValidadorSuma do
     context 'version 2.0' do
       context 'con datos' do
         context 'válido' do
-          it{ expect(ValidadorSuma.verificar_totales(Nokogiri::XML(File.read('spec/files/mensual_datos_2_0.xml')))).to eq(true) }
+          it{ expect(ValidadorSuma.validar(Nokogiri::XML(File.read('spec/files/mensual_datos_2_0.xml')))).to eq(true) }
         end
 
         context 'inválido' do
           let(:validador){}
-          it{ expect(ValidadorSuma.verificar_totales(Nokogiri::XML(File.read('spec/files/mensual_ceros_con_detalles_2_0.xml')))).to eq(false) }
+          it{ expect(ValidadorSuma.validar(Nokogiri::XML(File.read('spec/files/mensual_ceros_con_detalles_2_0.xml')))).to eq(false) }
         end
       end
 
       context 'sin datos' do
         context 'válido' do
-          it{ expect(ValidadorSuma.verificar_totales(Nokogiri::XML(File.read('spec/files/mensual_ceros_2_0.xml')))).to eq(true) }
+          it{ expect(ValidadorSuma.validar(Nokogiri::XML(File.read('spec/files/mensual_ceros_2_0.xml')))).to eq(true) }
         end
 
         context 'inválido' do
-          it{ expect(ValidadorSuma.verificar_totales(Nokogiri::XML(File.read('spec/files/mensual_datos_sin_detalles_2_0.xml')))).to eq(false) }
+          it{ expect(ValidadorSuma.validar(Nokogiri::XML(File.read('spec/files/mensual_datos_sin_detalles_2_0.xml')))).to eq(false) }
         end
       end
     end
@@ -29,21 +29,23 @@ describe ValidadorSuma do
     context 'version 1.1' do
       context 'con datos' do
         context 'válido' do
-          it{ expect(ValidadorSuma.verificar_totales(Nokogiri::XML(File.read('spec/files/mensual_datos_1_1.xml')))).to eq(true) }
+          it{ expect(ValidadorSuma.validar(Nokogiri::XML(File.read('spec/files/mensual_datos_1_1.xml')))).to eq(true) }
+          it{ expect(ValidadorSuma.validar(Nokogiri::XML(File.read('spec/files/anual_datos_1_1.xml')))).to eq(true) }
         end
 
         context 'inválido' do
-          it{ expect(ValidadorSuma.verificar_totales(Nokogiri::XML(File.read('spec/files/mensual_ceros_con_detalles_1_1.xml')))).to eq(false) }
+          it{ expect(ValidadorSuma.validar(Nokogiri::XML(File.read('spec/files/mensual_ceros_con_detalles_1_1.xml')))).to eq(false) }
         end
       end
 
       context 'sin datos' do
         context 'válido' do
-          it{ expect(ValidadorSuma.verificar_totales(Nokogiri::XML(File.read('spec/files/mensual_ceros_1_1.xml')))).to eq(true) }
+          it{ expect(ValidadorSuma.validar(Nokogiri::XML(File.read('spec/files/mensual_ceros_1_1.xml')))).to eq(true) }
+          it{ expect(ValidadorSuma.validar(Nokogiri::XML(File.read('spec/files/anual_ceros_1_1.xml')))).to eq(true) }
         end
 
         context 'inválido' do
-          it{ expect(ValidadorSuma.verificar_totales(Nokogiri::XML(File.read('spec/files/mensual_datos_sin_detalles_1_1.xml')))).to eq(false) }
+          it{ expect(ValidadorSuma.validar(Nokogiri::XML(File.read('spec/files/mensual_datos_sin_detalles_1_1.xml')))).to eq(false) }
         end
       end
     end
