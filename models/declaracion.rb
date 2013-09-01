19b9c606fd0a25ef757965a79abf586e923d8798
Azarel Doroteo Pacheco <azareldp@gmail.com>
2013-09-01 17:45:24 -0500

    Add more attributes to declaracion.

--- models/declaracion.rb
+++ models/declaracion.rb
@@ -1,5 +1,4 @@
 class Declaracion
-
   attr_reader :xml
 
   def initialize(xml)
@@ -15,6 +14,22 @@ class Declaracion
 
   def con_datos?
     !en_ceros?
+  end
+
+  def anual?
+    !!nodo_declaracion_anual
+  end
+
+  def mensual?
+    !!nodo_declaracion_mensual
+  end
+
+  def normal?
+    !!nodo_normal
+  end
+
+  def complementaria?
+    !!nodo_complementaria
   end
 
   def rfc
@@ -37,14 +52,38 @@ class Declaracion
     @version ||= nodo_declaracion['version']
   end
 
+  def ejercicio
+    @ejercicio ||= nodo_normal['ejercicio'] if nodo_normal
+  end
+
+  def periodo
+    @periodo ||= nodo_normal_o_complementaria['periodo']
+  end
+
   private #========================= PRIVATE ============================== #
 
   def nodo_declaracion
-    @declaracion_node ||= obtener_nodo_declaracion
+    @nodo_declaracion ||= (nodo_declaracion_anual || nodo_declaracion_mensual)
   end
 
-  def obtener_nodo_declaracion
-    xml.at_css('DeclaracionInformativaMensualIDE') ||
-      xml.at_css('DeclaracionInformativaAnualIDE')
+  def nodo_declaracion_anual
+    @nodo_declaracion_mensual ||= xml.at_css('DeclaracionInformativaAnualIDE')
   end
+
+  def nodo_declaracion_mensual
+    @nodo_declaracion_anual ||= xml.at_css('DeclaracionInformativaMensualIDE')
+  end
+
+  def nodo_normal_o_complementaria
+    nodo_normal || nodo_complementaria
+  end
+
+  def nodo_normal
+    @nodo_normal ||= xml.at_css('Normal')
+  end
+
+  def nodo_complementaria
+    @nodo_complementaria ||= xml.at_css('Complementaria')
+  end
+
 end
--- spec/declaracion_spec.rb
+++ spec/declaracion_spec.rb
@@ -7,19 +7,27 @@ describe Declaracion do
     context 'con datos' do
       subject {Declaracion.new(Nokogiri::XML(File.read('spec/files/mensual_datos_1_1.xml')))}
       it { should be_con_datos }
+      it { should be_normal }
+      it { should be_mensual }
       its(:numero_elementos) { should == 1 }
       its(:version) { should == "1.1" }
       its(:rfc) { should == "CAF020826YYY" }
       its(:denominacion) { should == "Caja Ficticia" }
+      its(:ejercicio) { should == "2012"}
+      its(:periodo) { should == "1"}
     end
 
     context 'en ceros' do
       subject {Declaracion.new(Nokogiri::XML(File.read('spec/files/mensual_ceros_1_1.xml')))}
       it { should be_en_ceros }
+      it { should be_normal }
+      it { should be_mensual }
       its(:numero_elementos) { should == 0 }
       its(:version) { should == "1.1" }
       its(:rfc) { should == "CAF020826YYY" }
       its(:denominacion) { should == "Caja Ficticia" }
+      its(:ejercicio) { should == "2011"}
+      its(:periodo) { should == "5"}
     end
   end
 
@@ -27,19 +35,27 @@ describe Declaracion do
     context 'con datos' do
       subject {Declaracion.new(Nokogiri::XML(File.read('spec/files/mensual_datos_2_0.xml')))}
       it { should be_con_datos }
+      it { should be_normal }
+      it { should be_mensual }
       its(:numero_elementos) { should == 3 }
       its(:version) { should == "2.0" }
       its(:rfc) { should == "AAA111111AB0" }
       its(:denominacion) { should == "Caja de Prueba S.A. de C.V." }
+      its(:ejercicio) { should == "2013"}
+      its(:periodo) { should == "7"}
     end
 
     context 'en ceros' do
       subject {Declaracion.new(Nokogiri::XML(File.read('spec/files/mensual_ceros_2_0.xml')))}
       it { should be_en_ceros }
+      it { should be_normal }
+      it { should be_mensual }
       its(:numero_elementos) { should == 0 }
       its(:version) { should == "2.0" }
       its(:rfc) { should == "AAA111111AB0" }
       its(:denominacion) { should == "Caja de Prueba S.A. de C.V." }
+      its(:ejercicio) { should == "2013"}
+      its(:periodo) { should == "7"}
     end
   end
 end
